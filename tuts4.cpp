1e66c40bbe55dfd2c5ce1a652e501e677f46b5ba
AbhilashjhaEEE <abhilashj466@gmail.com>
2026-05-18 17:35:28 +0530

    Create tuts4.cpp
    added for tuts4

---
+++ tuts4.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=5;
+    int b=6;
+    float pi=6.66;
+    char C ='5';
+
+    cout<<"Thish tutorial 4.\nere the value of a is "<<a<<" .\ne value of b is "<<b;
+    cout<<"the value of pi is "<<pi;
+    cout<<"the value of char is "<<C;
+    
+    return 0;
+}                                                      
